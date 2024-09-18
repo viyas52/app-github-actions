65926d27fb9a3d6c8357bf8f6c40432d0cefb83a
viyas52 <vyas.illusionist@gmail.com>
2024-09-18 23:34:49 +0530

    adding more test cases

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,8 +4,10 @@ def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
     assert add(3,7)==10
+    assert add(20,20)==40
     
 def test_sub():
     assert sub(2,3)==-1
     assert sub(-1,1)==-2
     assert sub(3,3)==0
+    assert sub(5,3)==2
